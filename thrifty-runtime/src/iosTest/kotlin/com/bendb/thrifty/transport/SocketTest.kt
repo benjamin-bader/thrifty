85749ce703e20bbfbc16144a9918367ab3dad8b4
Ben Bader <ben@bendb.com>
2023-09-23 00:10:17 -0600

    Add missing license headers

--- thrifty-runtime/src/iosTest/kotlin/com/bendb/thrifty/transport/SocketTest.kt
+++ thrifty-runtime/src/iosTest/kotlin/com/bendb/thrifty/transport/SocketTest.kt
@@ -1,29 +1,40 @@
+/*
+ * Thrifty
+ *
+ * Copyright (c) Benjamin Bader
+ * Copyright (c) Microsoft Corporation
+ *
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE,
+ * FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+ */
 package com.bendb.thrifty.transport
 
-import com.bendb.thrifty.binaryProtocol
 import com.bendb.thrifty.protocol.BinaryProtocol
 import com.bendb.thrifty.protocol.Xtruct
 import io.kotest.matchers.shouldBe
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.convert
 import okio.use
-import platform.Network.NW_PARAMETERS_DEFAULT_CONFIGURATION
-import platform.Network.NW_PARAMETERS_DISABLE_PROTOCOL
-import platform.Network.nw_connection_cancel
-import platform.Network.nw_connection_copy_endpoint
-import platform.Network.nw_connection_create
 import platform.Network.nw_connection_set_queue
 import platform.Network.nw_connection_set_state_changed_handler
 import platform.Network.nw_connection_start
 import platform.Network.nw_connection_state_cancelled
 import platform.Network.nw_connection_state_failed
 import platform.Network.nw_connection_state_ready
-import platform.Network.nw_connection_t
-import platform.Network.nw_endpoint_create_host
-import platform.Network.nw_endpoint_t
 import platform.Network.nw_listener_cancel
 import platform.Network.nw_listener_create
-import platform.Network.nw_listener_create_with_port
 import platform.Network.nw_listener_get_port
 import platform.Network.nw_listener_set_new_connection_handler
 import platform.Network.nw_listener_set_queue
@@ -32,25 +43,19 @@ import platform.Network.nw_listener_start
 import platform.Network.nw_listener_state_cancelled
 import platform.Network.nw_listener_state_failed
 import platform.Network.nw_listener_state_ready
-import platform.Network.nw_listener_t
 import platform.Network.nw_parameters_copy_default_protocol_stack
 import platform.Network.nw_parameters_create
-import platform.Network.nw_parameters_create_secure_tcp
-import platform.Network.nw_parameters_set_reuse_local_address
 import platform.Network.nw_protocol_stack_set_transport_protocol
 import platform.Network.nw_tcp_create_options
 import platform.Network.nw_tcp_options_set_connection_timeout
 import platform.Network.nw_tcp_options_set_enable_keepalive
-import platform.darwin.DISPATCH_QUEUE_SERIAL
 import platform.darwin.DISPATCH_TIME_FOREVER
-import platform.darwin.NSEC_PER_SEC
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_get_global_queue
 import platform.darwin.dispatch_queue_create
 import platform.darwin.dispatch_semaphore_create
 import platform.darwin.dispatch_semaphore_signal
 import platform.darwin.dispatch_semaphore_wait
-import platform.darwin.glob
 import platform.posix.QOS_CLASS_DEFAULT
 import kotlin.test.Test
 
